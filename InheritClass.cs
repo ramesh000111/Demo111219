0b1abd41d5eccaa6df87f5e1779c8fe065516253
rumkum <rumkum9882@gmail.com>
2019-11-12 15:17:39 +0530

    first commit

---
+++ InheritClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInheritanceConsoleApp1
+{
+    class InheritClass //: ProcessPhysics
+    {
+        ProcessPhysics process = new ProcessPhysics();
+       //string str = ProcessMath.().ToString();
+        public  void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.Beep(); //process.
+            Console.ReadKey();
+        }
+    }
+    class ProcessMath : InheritClass
+    {
+        public string Marks()
+        {
+            return "test Marks";
+        }
+    }
+    class ProcessHindi : ProcessMath
+    {
+        public string Marks()
+        {
+            return "test Marks2";
+        }
+    }
+    class ProcessEnglish : ProcessHindi
+    {
+        public string MarksEnglish()
+        {
+            return "test Marks English";
+        }
+    }
+    class ProcessPhysics : ProcessEnglish
+    {
+        public string MarksPhysics()
+        {
+            return "test Marks Physics";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInheritanceConsoleApp1
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // The code provided will print ‘Hello World’ to the console.
+    //        // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+    //        Console.WriteLine("Hello World!");
+    //        Console.ReadKey();
+
+    //        // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+    //    }
+    //}
+    public interface IProcessMath
+    {
+        string MarksMath();
+    }
+    public interface IProcessHindi
+    {
+        string MarksHindi();
+    }
+    public interface IProcessEnglish
+    {
+        string MarksEnglish();
+    }
+    public interface IProcessPhysics
+    {
+        string MarksPhysics();
+    }
+
+    // =====> Classess
+    class TestSubjectProcess   : IProcessHindi, IProcessEnglish, IProcessMath, IProcessPhysics
+    {
+
+        protected int x=1;
+        protected int y=11;
+        protected string txt="RCD";
+
+        public string MarksHindi()
+        {
+            return "Hindi=====> 4/10";
+        }
+        public string MarksEnglish()
+        {
+            return "English====> 7/10";
+        }
+        public string MarksMath()
+        {
+            return "Maths=====>9/10";
+        }
+
+        public string MarksPhysics()
+        {
+            return "Physics====>8/10";
+        }
+
+    }
+    //=====> Main class 
+    class Program //: TestSubjectProcess
+    {
+        public static void Main(string[] args)
+        {
+            TestSubjectProcess testSubject = new TestSubjectProcess();
+
+            string Eng = testSubject.MarksEnglish();
+            string Mat = testSubject.MarksMath();
+            string Hin = testSubject.MarksHindi();
+            string Phy = testSubject.MarksPhysics();
+
+            Console.WriteLine(Eng);
+            Console.WriteLine(Mat);
+            Console.WriteLine(Hin);
+            Console.WriteLine(Phy);
+
+            Console.ReadKey();  
+        }
+    }
+
+}
